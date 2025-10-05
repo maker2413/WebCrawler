ef082d1c4c7bf271514b24cd1846b273e9981cfc
maker2413 <maker2413@gmail.com>
2025-10-05 10:03:29 -0500

    reading command line arguments from main.go

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	rawBaseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
 }
